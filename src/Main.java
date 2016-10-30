0647f63231b6b5f7fa58c74187fa45f061626716
pawel <kolakpk@gmail.com>
2016-10-30 19:11:51 +0100

    jak działa okno w javie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
 /**
  * Created by pawel on 30.10.2016.
  */
-public class Main {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application{
+
+    Button button;
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("moje okno");
+        button = new Button();
+        button.setText("dupa");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 400, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 }
